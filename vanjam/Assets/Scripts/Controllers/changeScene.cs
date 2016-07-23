8e154695677bdfc6e9fe5d6ff5d720f577fcfff6
dannytaylor <dht@openmailbox.org>
2016-07-23 01:15:34 -0700

    some bounds but haven't implimented them yet

---
+++ vanjam/Assets/Scripts/Controllers/changeScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Are we colliding with something?
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //What did we collide with?
+        if (collision.gameObject.tag == Toolbox.TAG_PLAYER)
+        {
+            //Handle that here
+            
+        }
+    }
+
+    
+}
