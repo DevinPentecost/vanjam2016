7175e6d37262b0f42515597e4db6f2bf8c860d37
dannytaylor <dht@openmailbox.org>
2016-07-22 18:50:09 -0700

    Camera follows player to edge of level

--- vanjam/Assets/Standard Assets/Utility/FollowTarget.cs
+++ vanjam/Assets/Standard Assets/Utility/FollowTarget.cs
@@ -7,12 +7,18 @@ namespace UnityStandardAssets.Utility
     public class FollowTarget : MonoBehaviour
     {
         public Transform target;
-        public Vector3 offset = new Vector3(0f, 7.5f, 0f);
+        public Vector3 offset = new Vector3(0f, 0f, 0f);
+        public GameObject player;
 
+        // Distance from the center of the level
+        public float levelBound = 15;
 
         private void LateUpdate()
         {
-            transform.position = target.position + offset;
+            // Camera follows the player if it is within the level bounds
+            if(player.transform.position.x < levelBound & player.transform.position.x > -levelBound){
+                transform.position = target.position + offset;
+            }
         }
     }
 }
