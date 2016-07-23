ac9d93a073127c542360576135c0f299d27989a5
dpentecost <devinpentecost@gmail.com>
2016-07-22 18:07:34 -0700

    Devin Commit with basic movement

---
+++ vanjam/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//The velocity when walking
+	public float walkingSpeed = 0.5f;
+	private float currentSpeed = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Is the user pressing left?
+		this.currentSpeed = Input.GetAxis("Movement") * this.walkingSpeed;
+
+		//If we are moving...
+		if(this.currentSpeed != 0)
+		{
+			//Update our position
+			Vector3 currentPosition = this.transform.localPosition;
+			currentPosition.x += this.currentSpeed;
+
+			//Now set the position
+			this.transform.localPosition = currentPosition;
+		}
+	
+	}
+}
