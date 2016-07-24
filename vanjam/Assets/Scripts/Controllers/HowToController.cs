02dcede9965db5ddd6a713f67f075e63932d50b8
dannytaylor <dht@openmailbox.org>
2016-07-24 02:06:09 -0700

    quick level test and added start menu and howto page

---
+++ vanjam/Assets/Scripts/Controllers/HowToController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; // neded in order to load scenes
+
+public class HowToController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            StartLevel();
+        }
+
+    }
+    void StartLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
